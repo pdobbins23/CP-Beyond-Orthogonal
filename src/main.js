7f1e07d3e52b201bf4c4a35bf42d5fe5bf1031df
Nathan Altice <naltice@ucsc.edu>
2023-10-19 21:13:48 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Exercise 01: Normalized Movement
+// Name:
+// Date: 
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 800,
+    scene: [ Movement ]
+}
+
+let game = new Phaser.Game(config)
